1ebd3c62b0e6b3fa75d6a24495c3bc0abac4b951
ykaa <pepahurka3@gmail.com>
2026-05-23 16:03:55 +0200

    Update sv_logs.lua

--- server/sv_logs.lua
+++ server/sv_logs.lua
@@ -1,7 +1,7 @@
 local function SendWebhook(webhookUrl, payload)
     PerformHttpRequest(webhookUrl, function(statusCode, responseText)
         if statusCode ~= 204 then
-            print("^6[YKAA LOGS] ^3[WARNING] Webhook error)
+            print("^6[YKAA LOGS] ^3[WARNING] Webhook error")
         end
     end, "POST", json.encode(payload), { ["Content-Type"] = "application/json" })
 end
